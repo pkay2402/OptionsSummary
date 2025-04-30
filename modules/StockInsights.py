c8b86f3f6f09697a25a2270a950b051382bc6f52
pkay2402 <piyushkhaitan@gmail.com>
2025-04-30 18:34:45 -0500

    Update StockInsights.py

--- modules/StockInsights.py
+++ modules/StockInsights.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+import appdirs as ad
+ad.user_cache_dir = lambda *args: "/tmp"
 import yfinance as yf
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
